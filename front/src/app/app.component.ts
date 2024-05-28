535211e59c467fad56525d3d1beaa4d89df1afdb
Didier Barriere Doleac <didierdoleac@hotmail.com>
2024-05-28 15:28:57 +0200

    chore: Update toolbar and button styles in app component for not connected homepage

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from './pages/auth/services/auth.service';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { SessionService } from './services/session.service';
-import { Observable } from 'rxjs';
+import { Observable, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +10,16 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isHomePage$: Observable<boolean>;
+
   constructor(
     private authService: AuthService,
     private router: Router,
     private sessionService: SessionService) {
+      this.isHomePage$ = this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.router.url === '/' || this.router.url === '/login' || this.router.url === '/register')
+      );
   }
 
   public $isLogged(): Observable<boolean> {
