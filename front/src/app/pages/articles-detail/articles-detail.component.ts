daa7fa685eebdd377ae2d937960ad66140b47976
Didier Barriere Doleac <didierdoleac@hotmail.com>
2024-06-12 09:48:59 +0200

    chore: Update article detail page layout and styles

--- front/src/app/pages/articles-detail/articles-detail.component.ts
+++ front/src/app/pages/articles-detail/articles-detail.component.ts
@@ -4,6 +4,8 @@ import { Location } from '@angular/common';
 import { PostService } from 'src/app/services/post.service';
 import { PostInformation } from 'src/app/interfaces/postInformation.interface';
 import { SessionService } from 'src/app/services/session.service';
+import { SubscriptionInformation } from 'src/app/interfaces/subscriptionInformation.interface';
+import { SubscriptionService } from 'src/app/services/subscription.service';
 
 @Component({
   selector: 'app-articles-detail',
@@ -14,11 +16,13 @@ export class ArticlesDetailComponent implements OnInit {
   id: string | null;
   article: PostInformation | null;
   username: string = '';
+  topicName: string = '';
 
   constructor(
     private route: ActivatedRoute, 
     private location: Location,
     private sessionService: SessionService,
+    private subscriptionService: SubscriptionService,
     private postService: PostService
   ) {
     this.id = '';
@@ -33,13 +37,21 @@ export class ArticlesDetailComponent implements OnInit {
 
   getArticleById(): void {
     const userId = this.sessionService.getSessionInformation().id;
-    
   
     if (this.id) {
       this.postService.getPostsByUserSubscriptions(userId)
-      .subscribe((articles: PostInformation[]) => {
-        this.article = articles.find(article => article.id === +this.id!)  || null;
-      });
+        .subscribe((articles: PostInformation[]) => {
+          const article = articles.find(article => article.id === +this.id!);
+          if (article) {
+            this.subscriptionService.getSubscribedTopics(userId).subscribe((topics: SubscriptionInformation[]) => {
+              const topic = topics.find(topic => topic.topicId === article.topicId);
+              this.article = {...article};
+              this.topicName = topic ? topic.topicName : '';
+            });
+          } else {
+            this.article = null;
+          }
+        });
     }
   }
 
